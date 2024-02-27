7828fea0370ac2c393493f1ee4896de2936dccae
Breno Pavanelli <zayocomercial@gmail.com>
2024-02-26 21:53:54 -0300

    atividade 01 até a 05

---
+++ algoritmos-logicadeprog/python/atividade04.py
@@ -0,0 +1,6 @@
+print('Vamos calcular sua média')
+nota1 = input('Diga qual sua nota na primeira prova: ')
+nota2 = input('Diga qual sua nota na segunda prova: ')
+nota3 = input('Diga qual sua nota na terceira prova: ')
+média = (int(nota1) + int(nota2) + int(nota3)) / 3
+print(f'A sua média é {média}')
---
+++ algoritmos-logicadeprog/python/atividade05.py
@@ -0,0 +1,4 @@
+print('Vamos ver sua idade em dias!')
+idade = int(input('Quantos anos você tem?'))
+idade *= 365
+print(f'Você tem {idade} dias vividos!')
