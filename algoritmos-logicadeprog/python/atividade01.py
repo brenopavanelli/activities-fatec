2e3c9f4ebf866a785d1153fea1e8dd6458ac649f
Breno Pavanelli <zayocomercial@gmail.com>
2024-02-26 21:11:48 -0300

    atividade01

---
+++ algoritmos-logicadeprog/python/atividade01.py
@@ -0,0 +1,3 @@
+total = input('Quantos reais você tem?')
+saldo = int(total) * 100
+print('Você possuí ' + str(saldo) + ' centavos')
---
+++ algoritmos-logicadeprog/python/atividade02.py
@@ -0,0 +1,4 @@
+n1 = input('Digite um número para ser somado: ')
+n2 = input('Digite outro número para ser somado: ')
+soma = int(n1) + int(n2)
+print('A soma dos números escolhidos é: ' + str(soma))
