49986d4afb26d79d9108ccb6a5504e92b48dc1c0
Zayozin <zayocomercial@gmail.com>
2024-03-20 16:02:53 -0300

    -

---
+++ algoritmos-logicadeprog/python/tasks/scripts/homework01.py
@@ -0,0 +1,5 @@
+massa = int(input('Digite a massa do objeto: '))
+velocidade = int(input('Digite a velocidade tangencial do objeto: '))
+raio = int(input('Digite o raio do círculo: '))
+
+print(f'A força centrípeta é de: {(massa*velocidade**2) / raio}') 
