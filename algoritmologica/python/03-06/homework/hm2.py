1eaeec34399321f4faa12e949873178ba08a1fed
Zayozin <zayocomercial@gmail.com>
2024-06-04 10:22:59 -0300

    finish homeworks 03-06

---
+++ algoritmologica/python/03-06/homework/hm2.py
@@ -0,0 +1,7 @@
+contatos = ["Ana", "Bruno", "Carla", "Daniel", "Eva", "Fábio"]
+
+contatos[4] = input('Digite o nome do 5º contato: ')
+
+print(contatos)
+print(contatos[0])
+print(contatos[-1])
---
+++ algoritmologica/python/03-06/homework/hm3.py
@@ -0,0 +1,5 @@
+temperaturas = [28.5, 30.2, 26.8, 29.3, 31.1, 27.9, 29.7]
+
+i = int(input('Digite o índice da temperatura que deseja consultar (0 a 6): '))
+
+print(f'A temperatura do {i+1}º dia da semana foi de {temperaturas[i]}')
