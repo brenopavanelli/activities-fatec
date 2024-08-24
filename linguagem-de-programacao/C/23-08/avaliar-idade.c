03f34eb2787915b559b32ef707932610a7537d72
Breno Pavanelli <zayocomercial@gmail.com>
2024-08-23 21:49:00 -0300

    aula do dia 23/08

---
+++ linguagem-de-programacao/C/23-08/avaliar-idade.c
@@ -0,0 +1,10 @@
+int main() {
+    int idade = 18;
+
+    if (idade >= 18) {
+        printf("É maior de idade");
+    }
+    else {
+        printf("É menor de idade");
+    }
+}
---
+++ linguagem-de-programacao/C/23-08/teste-de-notas.c
@@ -0,0 +1,17 @@
+int main() {
+    int nota = 9;
+
+    if (nota <= 5) {
+        printf("precisa melhorar");
+    }
+    else if (nota < 5 && nota <= 8){
+        printf("esta indo bem");
+    }
+    else if (nota > 8) {
+        printf("muito bem");
+    }
+    else {
+        printf("Sem condicoes");
+    }
+}
+
