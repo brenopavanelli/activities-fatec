bed7f7d9b743062cdc9fcbf7b86894059f58bf59
Breno Pavanelli dos Santos <zayocomercial@gmail.com>
2025-08-31 22:54:16 -0300

    revisao de encapsulamento em C# iniciada

---
+++ C#-fatec/02encapsulamento/Program.cs
@@ -0,0 +1,6 @@
+﻿// Níveis de acesso 
+
+// public -> qualquer classe tem acesso
+// protected -> apenas as classes filha ou a própria classe tem acesso
+// private -> somente a própria classe tem acesso
+
