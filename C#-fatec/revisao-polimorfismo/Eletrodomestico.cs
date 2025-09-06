98e3023114bf115f6a1eb4212688c5e8a311a36b
Breno Pavanelli dos Santos <zayocomercial@gmail.com>
2025-09-06 15:32:45 -0300

    feat: revisao de polimorfismo

---
+++ C#-fatec/revisao-polimorfismo/Eletrodomestico.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace revisao_polimorfismo
+{
+    public abstract class Eletrodomestico
+    {
+        private string _nome;
+
+
+        public Eletrodomestico(string nome) // Construtor
+        {
+            this._nome = nome;
+
+        }
+
+        public abstract void Ligar(); // Método abstrato -> C# não obriga ter uma implementação logo de cara
+        public abstract void Desligar(); // Método abstrato
+
+
+    }
+}
